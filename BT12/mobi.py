fcbb9742ed34ea71d1e031cf1c6f24bf5b1c710e
Võ Anh Kiệt <anhkiet1227@gmail.com>
2023-06-10 11:53:12 +0700

    backup

--- BT12/mobi.py
+++ BT12/mobi.py
@@ -1,8 +1,24 @@
-string = "QflMn`fH,ZHVW^7c"
-flag = ""
-for i, char in enumerate(string):
-    tmp = ord(char)
-    if i < 8:
-        tmp -= 3
-    flag += chr(tmp+i)
-print(flag)
+# main function
+if __name__ == '__main__':
+    
+    # declare variable
+    string = "QflMn`fH,ZHVW^7c"
+    flag = ""
+    
+    # loop through string
+    for i, char in enumerate(string):
+        
+        # convert char to ascii
+        tmp = ord(char)
+        
+        # check if i is less than 8
+        if i < 8:
+            
+            # tmp minus 3
+            tmp -= 3
+            
+        # add char to flag
+        flag += chr(tmp+i)
+        
+    # print flag
+    print(flag)
